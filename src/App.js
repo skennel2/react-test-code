9fadcc842f10c2979893f352903abd23297fd1e2
skennel2 <skennel2@gmail.com>
2019-09-17 09:59:37 +0900

    RenderProps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import NumberPrinter from './RenderProps/NumberPrinterWithPrefix'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <NumberPrinter prefix='카운트:'></NumberPrinter>
       </div>
     )
   }
---
+++ src/RenderProps/NumberCounter.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class NumberCounter extends Component {
+    state = {
+        cnt: 0
+    }
+
+    handleClick(){
+        this.setState({
+            cnt: this.state.cnt + 1
+        });
+    }
+
+    render() {
+        return (
+            <div style={{ height: '100%' }} onClick={this.handleClick.bind(this)}>
+                {/* 동적으로 렌더링되는 로직을 props로 노출할 수 있다. */}
+                {this.props.render(this.state)}
+            </div>
+        )
+    }
+}
