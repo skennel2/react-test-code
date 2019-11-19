80e90a3a02cea94f7751f7b0e6585101d1554d6f
skennel2 <skennel2@gmail.com>
2019-11-19 09:39:51 +0900

    예제 수정

--- src/ref/RefTestComponent.jsx
+++ src/ref/RefTestComponent.jsx
@@ -8,12 +8,21 @@ export default class RefTestComponent extends Component {
         this.divRef = React.createRef();
     }
 
-    onClickButton(e) {
-        console.log(e)
+    componentDidMount() {
+        // current에 각각의 엘리먼트가 할당되어있다.
+        console.log(this.inputRef);
+        console.log(this.divRef);
+    }
+
+    onClickButtonInput(e) {
         //TODO: current의 의미는 무엇일까?
         this.inputRef.current.focus();
     }
-    
+
+    onClickButtonButton(e) {
+        this.divRef.current.focus();
+    }
+
     onFocusDiv() {
         console.log('div에 포커스');
     }
@@ -22,13 +31,20 @@ export default class RefTestComponent extends Component {
         return (
             <>
                 <div>
-                    <button onClick={this.onClickButton.bind(this)}>인풋에 포커스 주기</button>
-                    <input ref={this.inputRef} type={"text"} />
+                    <button onClick={this.onClickButtonInput.bind(this)}>인풋에 포커스 주기</button>
+                    <div>
+                        <input ref={this.inputRef} type={"text"} />
+                    </div>
                 </div>
-                <div 
-                    tabIndex={100} // 탭인덱스를 주면 div에도 포커스를 줄수 있고 onFocus이벤트도 사용할 수 있다. TODO: 왜?
-                    onFocus={this.onFocusDiv.bind(this)}>
-                    aaa
+
+                <div>
+                    <button onClick={this.onClickButtonButton.bind(this)}>div에 포커스 주기</button>
+                    <div
+                        ref={this.divRef}
+                        tabIndex={-1} // 탭인덱스를 주면 div에도 포커스를 줄수 있고 onFocus이벤트도 사용할 수 있다. TODO: 왜? tabIndex는 정확히 뭐지
+                        onFocus={this.onFocusDiv.bind(this)}>
+                        div의 영역
+                    </div>
                 </div>
             </>
         )
