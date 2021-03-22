b3549aeb9e5c23914f191e148c51198d098f81dd
skennel2 <skennel2@gmail.com>
2021-03-22 15:35:17 +0900

    state 변경 test

--- src/components/setState/SetStateTest.jsx
+++ src/components/setState/SetStateTest.jsx
@@ -6,7 +6,34 @@ import React, { Component } from 'react'
  * 
  * 이벤트 콜백에서 setState를 여러차례 호출하면 렌더링은 일괄처리되어 한번만 일어난다.
  */
+
 export default class SetStateTest extends Component {
+
+    state = {
+        toggleRender: false
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={() => {
+                    this.setState({
+                        toggleRender: !this.state.toggleRender
+                    })
+                }}>
+                    부모 state 변경
+                </button>
+                <SetStateChild onChange={() => {
+                    this.setState({
+                        toggleRender: !this.state.toggleRender
+                    })
+                }} />
+            </>
+        )
+    }
+}
+
+class SetStateChild extends Component {
     state = {
         numberValue: 0,
         stringValue: ''
@@ -29,9 +56,11 @@ export default class SetStateTest extends Component {
                             numberValue: this.state.numberValue + 1
                         })
                     }}>
-                        setState
+                        setState 연속 호출 number value
                     </button>
-                    {this.state.numberValue}
+                    <div>
+                        {this.state.numberValue}, {this.state.stringValue}
+                    </div>
                 </div>
                 <div>
                     <button onClick={() => {
@@ -43,9 +72,11 @@ export default class SetStateTest extends Component {
                             stringValue: '2'
                         })
                     }}>
-                        setState
+                        setState 연속 호출 string value
                     </button>
-                    {this.state.stringValue}
+                    <div>
+                        {this.state.numberValue}, {this.state.stringValue}
+                    </div>
                 </div>
                 <div>
                     <button onClick={() => {
@@ -57,9 +88,33 @@ export default class SetStateTest extends Component {
                             numberValue: 11
                         })
                     }}>
-                        setState
+                        setState 연속 호출 string value, number value
                     </button>
-                    {this.state.numberValue}, {this.state.stringValue}
+                    <div>
+                        {this.state.numberValue}, {this.state.stringValue}
+                    </div>
+                </div>
+
+                {/*
+                    이 케이스에서도 componentDidUpdate는 한번만 호출 
+                */}
+                <div>
+                    <button onClick={() => {
+                        this.setState({
+                            stringValue: '1'
+                        })
+
+                        this.props.onChange();
+
+                        this.setState({
+                            numberValue: 11
+                        })
+                    }}>
+                        이벤트 콜백 중간에 onChange로 부모 state 변경
+                    </button>
+                    <div>
+                        {this.state.numberValue}, {this.state.stringValue}
+                    </div>
                 </div>
             </>
         )
