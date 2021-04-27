5d72c923589727027aaf8232bc559e290c3fdf06
skennel2 <skennel2@gmail.com>
2021-04-27 10:08:37 +0900

    children 테스트

--- src/components/children/ChildrenTestComponent.jsx
+++ src/components/children/ChildrenTestComponent.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 
 export default class ChildrenTestComponent extends Component {
+    state = {
+        renderToggler: false,
+        staticState: 'test'
+    }
+
     render() {
         return (
             <div>
-                <HasChildren>
+                <button onClick={() => {
+                    this.setState({
+                        renderToggler: !this.state.renderToggler
+                    })
+                }}>
+                    렌더링!!
+                </button>
+
+                <hr />
+
+                <HasChildren value={this.state.staticState}>
                     <Children value={'Hello'} />
                     <Children value={'World'} />
                 </HasChildren>
 
-                
+                <hr />
+
+                {/*  
+                    props.children을 렌더링하지 않는 컴포넌트에 강제로 children을 할당하였을 경우
+                */}
                 <Children value={'Hello'}>
                     {/* 특별한 에러없이 렌더링되지 않는다. */}
                     <Children value={'world'} />
@@ -20,6 +39,14 @@ export default class ChildrenTestComponent extends Component {
 }
 
 class HasChildren extends Component {
+
+    componentDidUpdate(prevProps) {
+        // 렌더링될때마다 children의 내용은 동일해도 인스턴스는 바뀌는듯하다.
+        if (this.props.children !== prevProps.children) {
+            console.log('children이 바뀌었다?');
+        }
+    }
+
     render() {
         return (
             <div>
